1fab37b10cec9d9092f7f32f75a39d040c7e7226
Angelo D'Ambrosio <a.dambrosioMD@gmail.com>
2023-11-10 01:03:51 +0100

    Add options page functionality to save and load user settings

--- options.js
+++ options.js
@@ -1,3 +1,30 @@
+// In-page cache of the user's options
+const options = {};
+const settingsForm = document.getElementById("settingsForm");
+
+
+// Save settings to Chrome storage
+function saveSettings() {
+  options.apiKey = settingsForm.apiKey.value;
+  options.modelTemp = settingsForm.modelTemp.value;
+  chrome.storage.sync.set(options);
+}
+
+// Load settings from Chrome storage
+function loadSettings() {
+  chrome.storage.sync.get(['apiKey', 'modelTemp'], function(items) {
+    settingsForm.apiKey.value = items.apiKey || '';
+    settingsForm.modelTemp.value = items.modelTemp || '0.3';
+  });
+}
+
+// Call loadSettings when the options page is loaded
+document.addEventListener('DOMContentLoaded', loadSettings);
+
+// Add event listeners to the input fields to save settings when they change
+document.getElementById('settingsForm').addEventListener('change', saveSettings)
+
+
 // Logic to manage the API key and model temperature on the options page
 document.getElementById('settingsForm').addEventListener('submit', function(event) {
   // Prevent the form from being submitted normally
