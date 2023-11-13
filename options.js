a201e2c3f761d85034c1a34ce887d26f5ab476b4
Angelo D'Ambrosio <a.dambrosioMD@gmail.com>
2023-11-13 11:28:06 +0100

    Refactored options.js file to use custom slider
    plus, refactor to use jquery

--- options.js
+++ options.js
@@ -1,43 +1,43 @@
 // In-page cache of the user's options
 const settings = {};
-const settingsForm = document.getElementById("settingsForm");
-
 
 // Save settings to Chrome storage
 function saveSettings() {
-  settings.apiKey = settingsForm.apiKey.value;
-  settings.modelTemp = settingsForm.modelTemp.value;
-
+  settings.apiKey = $('#apiKey').val();
+  settings.modelTemp = $('#modelTemp').val();
+  
   console.log(settings)
   chrome.storage.sync.set(settings);
 }
 
-// // Load settings from Chrome storage
-// async function loadSettings() {
-//   const settings = await chrome.storage.sync.get("settings");
-//   settingsForm.apiKey.value = settings.apiKey || '';
-//   settingsForm.modelTemp.value = settings.modelTemp || '0.3';
-
-//   return settings;
-// }
-
-// // Call loadSettings when the options page is loaded
-// document.addEventListener('DOMContentLoaded', loadSettings);
-
-// // Add event listeners to the input fields to save settings when they change
-// document.getElementById('settingsForm').addEventListener('change', saveSettings)
-
 // Initialize the settings in the popup
 async function loadSettings() {
-
+  
   const settings = await chrome.storage.sync.get();
-  document.getElementById('apiKey').value = settings.apiKey || '';
-  document.getElementById('modelTemp').value = settings.modelTemp || '0.3';
+  $('#apiKey').val(settings.apiKey || '');
+  $('#modelTemp').val(settings.modelTemp || '0.3');
+  setupSlider();
+  console.log("load settings:", $('#modelTemp').val())
   return settings;
 }
 
-// Call loadSettings when the options page is loaded
-document.addEventListener('DOMContentLoaded', loadSettings);
+$(document).ready(function () {
 
-// Add event listeners to the input fields to save settings when they change
-document.getElementById('settingsForm').addEventListener('change', saveSettings)
+  // Call loadSettings when the options page is loaded
+  loadSettings()
+  
+  // Add event listeners to the input fields to save settings when they change
+  $('#Settings input').on('change', saveSettings)
+  
+  
+  const extensionSwitch = $('#extensionSwitch');
+  const activationStatus = $('.activationStatus');
+  
+  extensionSwitch.on('input', () => {
+    if (extensionSwitch.checked) {
+      activationStatus.textContent = 'Active';
+    } else {
+      activationStatus.textContent = 'Off';
+    }
+  });
+});
